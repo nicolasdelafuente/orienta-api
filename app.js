0306d87c28a75e5341dc2056023b28f7d512313f
nicolas.delafuente <ni.delafuente@gmail.com>
2024-02-17 10:35:30 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor iniciado en el puerto ${port}`);
+});
