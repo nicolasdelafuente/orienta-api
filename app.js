be9b11933bbc6a815aaf7b16dd18d2d467deea14
nicolas.delafuente <ni.delafuente@gmail.com>
2024-02-17 10:51:16 -0300

    Add variables de entorno

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
 app.use(cors());
 
-const port = 3000;
+const port = process.env.PORT || 4000;
+const publicUrl = process.env.PUBLIC_URL || 'http://localhost:'
 
 app.listen(port, () => {
-    console.log(`The server has started successfully at http://localhost:${port}`);
+    console.log(`The server has started successfully at ${publicUrl}${port}`);
 });
