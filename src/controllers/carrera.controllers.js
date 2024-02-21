0843406ab79cebc7f2deb27d475b4c80ed4c2198
nicolas.delafuente <ni.delafuente@gmail.com>
2024-02-21 07:55:31 -0300

    Add endpoint update Carreras

--- src/controllers/carrera.controllers.js
+++ src/controllers/carrera.controllers.js
@@ -1,5 +1,5 @@
 const { Carrera } = require("../models");
-const logSqlErrorAndQuery  = require('../helpers/logSqlError.js');
+const logSqlErrorAndQuery = require('../helpers/logSqlError.js');
 const { handleHttpError } = require("../utils/handleError.js");
 
 const attributes = [
@@ -7,10 +7,10 @@ const attributes = [
     "nombre",
     "createdAt",
     "updatedAt"
-]
+];
 
 /**
- * Obtener una lista de carreras..
+ * Obtener una lista de todas las carreras.
  * @param {Object} req - Objeto de solicitud de Express.
  * @param {Object} res - Objeto de respuesta de Express utilizado para enviar la respuesta.
  */
@@ -18,12 +18,11 @@ const getItems = async (req, res) => {
     try {
         const data = await Carrera.findAll();
         res.send({ data });
-    } catch (e) {  
-        logSqlErrorAndQuery(e); 
-        handleHttpError(res, e.message, 'ERROR_GET_ITEMS', 403);
+    } catch (error) {
+        logSqlErrorAndQuery(error); 
+        handleHttpError(res, error.message, 'ERROR_GET_ITEMS', 500);
     }
 };
-
 
 /**
  * Obtener una carrera por su ID.
@@ -32,30 +31,51 @@ const getItems = async (req, res) => {
  */
 const getItem = async (req, res) => {
     try {
-        const id = req.params.id;
-        data = await Carrera.findByPk(id);
+        const { id } = req.params;
+        const data = await Carrera.findByPk(id);
+        if (!data) {
+            return res.status(404).json({ error: 'Carrera no encontrada' });
+        }
         res.send({ data });
-
-    } catch (e) {
-        logSqlErrorAndQuery(e); 
-        handleHttpError(res, e.message, 'ERROR_GET_ITEM', 403);
+    } catch (error) {
+        logSqlErrorAndQuery(error); 
+        handleHttpError(res, error.message, 'ERROR_GET_ITEM', 500);
     }
-}
-
+};
 
 /**
- * Obtener un registro de Base de datos
- * @param {*} req 
- * @param {*} res 
+ * Crear un nuevo registro de carrera en la base de datos.
+ * @param {Object} req - El objeto de solicitud HTTP que debe contener los datos de la carrera a crear.
+ * @param {Object} res - El objeto de respuesta HTTP.
  */
 const createItem = async (req, res) => {
     try {
-        const data = await Carrera.create({ req });
-        res.send({ data });
-    } catch (e) {
-        logSqlErrorAndQuery(e); 
-        handleHttpError(res, e.message, 'ERROR_CREATE_ITEMS', 403)
+        const data = await Carrera.create(req.body);
+        res.status(201).json({ success: true, data });
+    } catch (error) {
+        logSqlErrorAndQuery(error); 
+        handleHttpError(res, error.message, 'ERROR_CREATE_ITEM', 500);
     }
-}
+};
 
-module.exports = { getItems, getItem, createItem };
+/**
+ * Actualizar un registro de carrera en la base de datos.
+ * @param {Object} req - El objeto de solicitud HTTP que debe contener el ID de la carrera y los nuevos datos.
+ * @param {Object} res - El objeto de respuesta HTTP.
+ */
+const updateItem = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const carrera = await Carrera.findByPk(id);
+        if (!carrera) {
+            return res.status(404).json({ error: 'Carrera no encontrada' });
+        }
+        const updatedCarrera = await carrera.update(req.body);
+        res.status(200).json({ success: true, data: updatedCarrera });
+    } catch (error) {
+        logSqlErrorAndQuery(error); 
+        handleHttpError(res, error.message, 'ERROR_UPDATE_ITEM', 500);
+    }
+};
+
+module.exports = { getItems, getItem, createItem, updateItem };
--- src/routes/carreras.routes.js
+++ src/routes/carreras.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getItems, getItem, createItem } = require("../controllers/carrera.controllers.js");
+const { getItems, getItem, createItem, updateItem} = require("../controllers/carrera.controllers.js");
 
 router.get("/", getItems);
 router.get("/:id", getItem);
 router.post("/", createItem);
+router.put("/:id", updateItem);
 
 module.exports = router;
